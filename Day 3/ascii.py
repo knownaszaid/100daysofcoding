ef6c3a108b4b73f97f60eb313d5d3df8607208d4
Shaikh Mohammed Zaid Abdul Rahman <77042775+knownaszaid@users.noreply.github.com>
2022-11-20 18:13:23 +0530

    Add files via upload

---
+++ Day 3/ascii.py
@@ -0,0 +1,19 @@
+"""Day 3 coding Statement: Write a program to find ASCII values of a character
+
+ 
+
+Description: Get an input character from the user and give the ASCII value of the given input as the output.
+
+Input
+b
+B
+Output
+98
+66
+
+"""
+
+
+inp = input("Enter the Input:- \n")
+
+print(ord(inp))
